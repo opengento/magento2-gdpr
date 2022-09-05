d3bcd3b57df901969d96a410fc4cdc1675ad0d9a
Thomas Klein <thomas.klein@mpbio.com>
2022-09-05 22:46:01 +0200

    Remove deprecated CMS Block reference

--- ViewModel/Customer/Privacy/ExportDataProvider.php
+++ ViewModel/Customer/Privacy/ExportDataProvider.php
@@ -7,7 +7,6 @@ declare(strict_types=1);
 
 namespace Opengento\Gdpr\ViewModel\Customer\Privacy;
 
-use Magento\Cms\Block\Block;
 use Magento\Cms\Block\BlockByIdentifier;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
@@ -22,9 +21,6 @@ final class ExportDataProvider implements ArgumentInterface
 
     private BlockFactory $blockFactory;
 
-    /**
-     * @var string|null
-     */
     private ?string $exportInformation;
 
     public function __construct(
